5a2cfb083a300065e219ff55add263a27b88ece0
raimund89 <raimund89@gmail.com>
2015-09-20 21:31:54 +0200

    Adding package setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='DiNoLog',
+      version='0.1',
+      description='Distributed Node Logging for Python',
+      url='http://github.com/raimund89/DiNoLog',
+      author='Raimond Frentrop',
+      author_email='raimund89@gmail.com',
+      license='GPLv2',
+      packages=['DiNoLog'],
+      zip_safe=False)
