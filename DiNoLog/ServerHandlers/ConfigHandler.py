5fadfac4dcab019999d8ac6c20f9f7b9ef0ee0e8
raimund89 <raimund89@gmail.com>
2015-09-21 00:50:07 +0200

    Adding basic config handler, and a first attempt at error handling

--- DiNoLog/ServerHandlers/ConfigHandler.py
+++ DiNoLog/ServerHandlers/ConfigHandler.py
@@ -19,8 +19,35 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
+import configparser
 
-class ConfigHandler():
+
+class ConfigHandler(configparser.ConfigParser):
 
     def __init__(self):
-        pass
+        super().__init__()
+
+        # TODO: add other ways to specify the location of the config file
+        # For now, the config file is in the current directory
+        try:
+            self.read_file(open('dinologserver.conf'))
+            self.hasfile = True
+        except IOError:
+            # The config file does not exist. Quit with the message
+            # that the user should either specify the file or put it in
+            # the right place. The server can work with an empty file,
+            # but not with a nonexistant file
+            print("ERROR: The file 'dinologserver.conf' does not exist")
+            print("       It should exist before starting the DiNoLogServer")
+            print("       You can find a sample config in the documentation")
+
+            self.hasfile = False
+
+    def status(self):
+        '''Returns the status of the handler.'''
+        # Status can be True or False. If False, an additional string will
+        # specify what exactly is the problem
+        if not self.hasfile:
+            return {'code': False, 'reason': 'Could not open config file'}
+        else:
+            return {'code': True, 'reason': ''}
